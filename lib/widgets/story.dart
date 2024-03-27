d549801a6c18c2a1d19dba343760a0bf4dc2e05a
Misael Abanto <josemisaelabanto@gmail.com>
2024-03-26 23:49:01 -0500

    feat(widgets): add 'prefix' and 'isFinalFolder' parameters to Story widget constructor
    Added 'prefix' and 'isFinalFolder' parameters to the Story widget constructor to enable customization when creating the widget. This enhancement allows for more flexible usage of the Story widget with different configurations.

--- lib/widgets/story.dart
+++ lib/widgets/story.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:kaliman_reader_app/providers/leading_image.dart';
 
 class Story extends StatelessWidget {
-  final String? title;
+  final String title;
   final GestureTapCallback? onTap;
+  final String prefix;
+  final bool isFinalFolder;
 
-  const Story({Key? key, this.title, this.onTap}) : super(key: key);
+  const Story(
+      {Key? key,
+      required this.title,
+      this.onTap,
+      required this.prefix,
+      required this.isFinalFolder})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title!),
+      title: Text(title),
+      leading: Image(image: LeadingImage(prefix, isFinalFolder: isFinalFolder)),
       onTap: onTap,
     );
   }
