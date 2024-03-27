88a667721326d6646463a50cf20ee8c1370a7ec1
Misael Abanto <josemisaelabanto@gmail.com>
2024-03-26 23:47:54 -0500

    feat(common): add GlobalKey declaration for ScaffoldMessengerState instance
    Adding import statement and declaring a GlobalKey for ScaffoldMessengerState to facilitate global access within the application.

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final String? apiUrl = dotenv.env['API_URL'];
+final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
