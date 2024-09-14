6a2f3dde8939ccd6c4d933b73032e61adde6f179
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-13 21:21:51 -0500

    feat(backend/widgets): add ad banner widget for displaying ads
    - Added AdBanner widget for banner advertisements
    - Integrated Google Mobile Ads package and dotenv for ad unit ID
    - Implemented ad loading with proper state management
    - Included event listeners for ad lifecycle handling

---
+++ lib/widgets/ad_banner.dart
@@ -0,0 +1,81 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdBanner extends StatefulWidget {
+  const AdBanner({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AdBannerState();
+}
+
+class _AdBannerState extends State<AdBanner> {
+  BannerAd? _bannerAd;
+  bool _isBannerLoaded = false;
+
+  final String _adUnitId = dotenv.get('AD_BANNER_UNIT_ID');
+
+  @override
+  Widget build(BuildContext context) {
+    _loadAd();
+    if (_bannerAd != null && _isBannerLoaded) {
+      return Align(
+        alignment: Alignment.bottomCenter,
+        child: SafeArea(
+          child: Ink(
+            color: Color(Theme.of(context).scaffoldBackgroundColor.value),
+            child: SizedBox(
+              width: _bannerAd!.size.width.toDouble(),
+              height: _bannerAd!.size.height.toDouble(),
+              child: AdWidget(ad: _bannerAd!),
+            ),
+          ),
+        ),
+      );
+    } else {
+      return SizedBox(
+        width: AdSize.banner.width.toDouble(),
+        height: AdSize.banner.height.toDouble(),
+      );
+    }
+  }
+
+  void _loadAd() async {
+    if (!mounted) {
+      return;
+    }
+
+    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
+    const size = AdSize.banner;
+
+    BannerAd(
+      adUnitId: _adUnitId,
+      request: const AdRequest(),
+      size: size,
+      listener: BannerAdListener(
+        // Called when an ad is successfully received.
+        onAdLoaded: (ad) {
+          setState(() {
+            _bannerAd = ad as BannerAd;
+            _isBannerLoaded = true;
+          });
+        },
+        // Called when an ad request failed.
+        onAdFailedToLoad: (ad, err) {
+          log('Ad failed to load: $err');
+          ad.dispose();
+        },
+        // Called when an ad opens an overlay that covers the screen.
+        onAdOpened: (Ad ad) {
+          log('Ad opened.');
+        },
+        // Called when an ad removes an overlay that covers the screen.
+        onAdClosed: (Ad ad) {},
+        // Called when an impression occurs on the ad.
+        onAdImpression: (Ad ad) {},
+      ),
+    ).load();
+  }
+}
