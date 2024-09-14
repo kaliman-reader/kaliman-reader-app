eeffb3e897f45fa4aa9b4d2b8558ed53210fd891
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-13 21:36:48 -0500

    feat(macos/plugins): add new plugins for path and sharing
    - Added path_provider_foundation plugin to handle filesystem paths
    - Integrated share_plus plugin to add sharing capabilities
    - Registered new plugins in the macOS generated plugin registry

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import path_provider_foundation
+import share_plus
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
