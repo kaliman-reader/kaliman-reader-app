3b65ccb6ec6a59391cdfcc301a9ad0a287d2b187
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-17 00:09:46 -0500

    feat(backend/subfolder): integrate analytics and refactor state management
    - Added Firebase Analytics screen view logging in initState
    - Refactored SubFolderPage to include additional prefix parameter
    - Moved from ListView to ListView.builder for prefix items rendering
    - Adjusted padding to separate content from bottom banner AdBanner

--- lib/pages/subfolder.dart
+++ lib/pages/subfolder.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:kaliman_reader_app/models/prefix.dart';
 import 'package:kaliman_reader_app/pages/reader_page.dart';
@@ -7,7 +8,10 @@ import '../widgets/story.dart';
 
 class SubFolderPage extends StatefulWidget {
   final List<Prefix> prefixes;
-  const SubFolderPage({super.key, required this.prefixes});
+  final Prefix prefix;
+
+  const SubFolderPage(
+      {super.key, required this.prefixes, required this.prefix});
   @override
   State<StatefulWidget> createState() => _SubFolderPageState();
 }
@@ -19,31 +23,44 @@ class _SubFolderPageState extends State<SubFolderPage> {
   }
 
   @override
+  void initState() {
+    FirebaseAnalytics.instance.logScreenView(
+      screenName: 'subfolder_page',
+      screenClass: 'SubFolderPage',
+      parameters: {'prefix': widget.prefixes[0].prefix},
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Capítulos"),
-        ),
-        body: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView(
-                children: widget.prefixes.map((e) {
-                  return Story(
-                    title: e.prefix.replaceAllMapped(
-                        RegExp(r'\/(.*)\/'), (match) => ' (${match[1]})'),
-                    onTap: () {
-                      goToReaderPage(context, e.prefix);
-                    },
-                    prefix: e.prefix,
-                    isFinalFolder: true,
-                  );
-                }).toList(),
-              ),
+      appBar: AppBar(
+        title: const Text("Capítulos"),
+      ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 60),
+            child: ListView.builder(
+              itemCount: widget.prefixes.length,
+              itemBuilder: (context, index) {
+                final prefix = widget.prefixes[index].prefix;
+                return Story(
+                  title: prefix.replaceAllMapped(
+                      RegExp(r'\/(.*)\/'), (match) => ' (${match[1]})'),
+                  onTap: () {
+                    goToReaderPage(context, prefix);
+                  },
+                  prefix: prefix,
+                  isFinalFolder: true,
+                );
+              },
             ),
-            const AdBanner(),
-          ],
-        ));
+          ),
+          const AdBanner(),
+        ],
+      ),
+    );
   }
 }
