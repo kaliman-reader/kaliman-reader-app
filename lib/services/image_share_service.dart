19e7bb655216afb5d6d318e6b80316ecd70e672a
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-17 00:09:58 -0500

    feat(backend/services): add analytics event for image sharing
    - Added Firebase Analytics event logging to track image share actions
    - The 'download_page' event logs the key parameter for better analysis

--- lib/services/image_share_service.dart
+++ lib/services/image_share_service.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:http/http.dart';
 import 'package:kaliman_reader_app/repositories/object_repository.dart';
 import 'package:share_plus/share_plus.dart';
@@ -6,6 +7,12 @@ class ImageShareService {
   static Future<void> shareImage(String key) async {
     var object = await ObjectRepository.getObject(key);
     var response = await get(Uri.parse(object.url));
+    FirebaseAnalytics.instance.logEvent(
+      name: 'download_page',
+      parameters: {
+        'key': key,
+      },
+    );
     await Share.shareXFiles(
       [XFile.fromData(response.bodyBytes)],
       text: '¡Mira esta increíble página de Kaliman!',
