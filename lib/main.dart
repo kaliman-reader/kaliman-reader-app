cbb485eed2ad5ba3a55778db54497a001cd3defc
Misael Abanto <josemisaelabanto@gmail.com>
2024-09-17 00:10:38 -0500

    feat(frontend): integrate Firebase and enhance error logging
    - Added Firebase initialization with default options
    - Implemented Firebase Analytics for screen views and error logging
    - Minor refactoring of GridView and goToSubFolderPage function
    - Fixed typo in widget import: the-app-drawer to the_app_drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'dart:developer';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:kaliman_reader_app/common/constants.dart';
+import 'package:kaliman_reader_app/firebase_options.dart';
 import 'package:kaliman_reader_app/pages/subfolder.dart';
 import 'package:kaliman_reader_app/repositories/prefix_repository.dart';
 import 'package:kaliman_reader_app/widgets/ad_banner.dart';
 import 'package:kaliman_reader_app/widgets/grid_story.dart';
-import 'package:kaliman_reader_app/widgets/the-app-drawer.dart';
+import 'package:kaliman_reader_app/widgets/the_app_drawer.dart';
 
 import 'models/prefix.dart';
 
@@ -16,13 +19,15 @@ Future main() async {
   await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -56,6 +61,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     getPrefixes(Prefix(prefix: ""));
+    FirebaseAnalytics.instance.logScreenView(
+      screenName: 'home_page',
+      screenClass: 'MyHomePage',
+    );
     super.initState();
   }
 
@@ -72,16 +81,24 @@ class _MyHomePageState extends State<MyHomePage> {
         firstLevelPrefixes = prefixes;
       });
     } catch (exception) {
+      FirebaseAnalytics.instance.logEvent(name: 'error', parameters: {
+        'error': exception.toString(),
+        'stack_trace': Error().stackTrace.toString(),
+        'prefix': prefix.prefix
+      });
       log(exception.toString(),
           name: 'app.openlinks.kaliman_reader_app.prefixes', error: exception);
     }
   }
 
-  void goToSubFolderPage(List<Prefix> prefixes) {
+  void goToSubFolderPage(Prefix prefix, List<Prefix> prefixes) {
     Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) => SubFolderPage(prefixes: prefixes)));
+            builder: (context) => SubFolderPage(
+                  prefixes: prefixes,
+                  prefix: prefix,
+                )));
   }
 
   @override
@@ -96,38 +113,44 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Stack(
             children: [
               Center(
-                  child: SizedBox(
-                width: 20.0,
-                height: 20.0,
-                child:
-                    _loading == true ? const CircularProgressIndicator() : null,
-              )),
-              GridView(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2, mainAxisSpacing: 40),
-                  padding: const EdgeInsets.only(bottom: 50),
-                  children: firstLevelPrefixes.map((e) {
-                    return GridStory(
-                        title: e.prefix.replaceAll(RegExp(r'\/'), ''),
-                        prefix: e.prefix,
-                        isFinalFolder: false,
-                        onTap: () async {
-                          setLoading(true);
-                          try {
-                            var prefixes =
-                                await PrefixRepository.getPrefixes(e.prefix);
-                            goToSubFolderPage(prefixes);
-                          } catch (err) {
-                            var state = scaffoldMessengerKey.currentState;
-                            state?.showSnackBar(const SnackBar(
-                              content: Text(
-                                  '¡Pronto tendremos más novedades para ti!'),
-                            ));
-                          } finally {
-                            setLoading(false);
-                          }
-                        });
-                  }).toList()),
+                child: SizedBox(
+                  width: 20.0,
+                  height: 20.0,
+                  child: _loading == true
+                      ? const CircularProgressIndicator()
+                      : null,
+                ),
+              ),
+              GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, mainAxisSpacing: 40),
+                padding: const EdgeInsets.only(bottom: 100),
+                itemCount: firstLevelPrefixes.length,
+                itemBuilder: (context, index) {
+                  final currentPrefix = firstLevelPrefixes[index];
+                  return GridStory(
+                    title: currentPrefix.prefix.replaceAll(RegExp(r'\/'), ''),
+                    prefix: currentPrefix.prefix,
+                    isFinalFolder: false,
+                    onTap: () async {
+                      setLoading(true);
+                      try {
+                        var prefixes = await PrefixRepository.getPrefixes(
+                            currentPrefix.prefix);
+                        goToSubFolderPage(currentPrefix, prefixes);
+                      } catch (err) {
+                        var state = scaffoldMessengerKey.currentState;
+                        state?.showSnackBar(const SnackBar(
+                          content:
+                              Text('¡Pronto tendremos más novedades para ti!'),
+                        ));
+                      } finally {
+                        setLoading(false);
+                      }
+                    },
+                  );
+                },
+              ),
               const AdBanner(),
             ],
           )),
