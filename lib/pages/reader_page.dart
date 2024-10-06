5547cedd8d13770903884118a7a9f7c79899b8cf
Misael Abanto <josemisaelabanto@gmail.com>
2024-10-05 23:41:20 -0500

    feat(reader): enhance UI with ads and picture index in toolbar
    - Added an ad banner widget to the reader page layout
    - Displayed current picture index in the app bar for easy reference
    - Improved UI responsiveness with a column layout in reader page
    - Updated version in pubspec.yaml to '2.1.0+9'

--- lib/pages/reader_page.dart
+++ lib/pages/reader_page.dart
@@ -12,6 +12,7 @@ import 'package:kaliman_reader_app/repositories/object_key_repository.dart';
 import 'package:kaliman_reader_app/services/image_download_service.dart';
 import 'package:kaliman_reader_app/services/image_share_service.dart';
 import 'package:kaliman_reader_app/services/picture_sorter.dart';
+import 'package:kaliman_reader_app/widgets/ad_banner.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
@@ -73,6 +74,7 @@ class _ReaderPageState extends State<ReaderPage> {
     return Scaffold(
       appBar: _showAppBar
           ? AppBar(
+              title: Text('$currentPictureIndex/${pictureKeys.length}'),
               actions: [
                 IconButton(
                   icon: Icon(downloadIcon),
@@ -96,7 +98,7 @@ class _ReaderPageState extends State<ReaderPage> {
                   onPressed: () async {
                     await ImageShareService.shareImage(currentPictureUrl!);
                   },
-                )
+                ),
               ],
             )
           : null,
@@ -113,49 +115,54 @@ class _ReaderPageState extends State<ReaderPage> {
                     _loading == true ? const CircularProgressIndicator() : null,
               ),
             ),
-            PhotoViewGallery.builder(
-              scrollPhysics: const BouncingScrollPhysics(),
-              builder: (BuildContext context, int index) {
-                if (index + 1 < pictureKeys.length) {
-                  precacheImage(
-                      PictureKeyImage(pictureKeys[index + 1].key), context);
-                }
-                return PhotoViewGalleryPageOptions(
-                  imageProvider: PictureKeyImage(pictureKeys[index].key),
-                  initialScale: PhotoViewComputedScale.contained,
-                  onTapUp: (context, details, controllerValue) => setState(
-                    () => _showAppBar = !_showAppBar,
-                  ),
-                  heroAttributes:
-                      PhotoViewHeroAttributes(tag: pictureKeys[index].key),
-                );
-              },
-              itemCount: pictureKeys.length,
-              onPageChanged: (index) {
-                FirebaseAnalytics.instance.logScreenView(
-                  screenName: 'subfolder_page',
-                  screenClass: 'SubFolderPage',
-                  parameters: {'prefix': pictureKeys[index].key},
-                );
-                setState(() {
-                  currentPictureUrl = pictureKeys[index].key;
-                  currentPictureIndex = index;
-                  downloadIcon = Icons.download;
-                });
-              },
-              loadingBuilder: (context, event) => Center(
-                child: SizedBox(
-                  width: 20.0,
-                  height: 20.0,
-                  child: CircularProgressIndicator(
-                    value: event == null
-                        ? 0
-                        : event.cumulativeBytesLoaded /
-                            event.expectedTotalBytes!,
+            Column(children: [
+              Expanded(
+                child: PhotoViewGallery.builder(
+                  scrollPhysics: const PageScrollPhysics(),
+                  builder: (BuildContext context, int index) {
+                    if (index + 1 < pictureKeys.length) {
+                      precacheImage(
+                          PictureKeyImage(pictureKeys[index + 1].key), context);
+                    }
+                    return PhotoViewGalleryPageOptions(
+                      imageProvider: PictureKeyImage(pictureKeys[index].key),
+                      initialScale: PhotoViewComputedScale.contained,
+                      onTapUp: (context, details, controllerValue) => setState(
+                        () => _showAppBar = !_showAppBar,
+                      ),
+                      heroAttributes:
+                          PhotoViewHeroAttributes(tag: pictureKeys[index].key),
+                    );
+                  },
+                  itemCount: pictureKeys.length,
+                  onPageChanged: (index) {
+                    FirebaseAnalytics.instance.logScreenView(
+                      screenName: 'reader_page',
+                      screenClass: 'ReaderPage',
+                      parameters: {'prefix': pictureKeys[index].key},
+                    );
+                    setState(() {
+                      currentPictureUrl = pictureKeys[index].key;
+                      currentPictureIndex = index;
+                      downloadIcon = Icons.download;
+                    });
+                  },
+                  loadingBuilder: (context, event) => Center(
+                    child: SizedBox(
+                      width: 20.0,
+                      height: 20.0,
+                      child: CircularProgressIndicator(
+                        value: event == null
+                            ? 0
+                            : event.cumulativeBytesLoaded /
+                                event.expectedTotalBytes!,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+              const AdBanner(),
+            ])
           ],
         ),
       ),
