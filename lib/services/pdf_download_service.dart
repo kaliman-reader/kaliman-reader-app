7db9b05391d4137931a9cda081fabab47c0edc3c
Misael Abanto <josemisaelabanto@gmail.com>
2024-10-16 00:20:43 -0500

    refactor(services/pdf_download): improve PDF download handling
    - Replaced ObjectRepository with PdfRepository for handling PDFs
    - Cleaned up prefix by replacing '/' with '-' to avoid path issues
    - Adjusted file path generation to ensure uniformity
    - Ensured consistent file naming with '.pdf' extension
    - Removed trailing slash in Android download path for consistency

--- lib/services/pdf_download_service.dart
+++ lib/services/pdf_download_service.dart
@@ -2,17 +2,18 @@ import 'dart:io';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:http/http.dart' show get;
-import 'package:kaliman_reader_app/repositories/object_repository.dart';
+import 'package:kaliman_reader_app/repositories/export_repository.dart';
 import 'package:path_provider/path_provider.dart';
 
 class PdfDownloadService {
   static Future<String> downloadPdf(String prefix) async {
-    var object = await ObjectRepository.getObject(prefix);
-    var response = await get(Uri.parse(object.url));
+    var cleanPrefix = prefix.replaceAll('/', '-');
+    var pdf = await PdfRepository.getPdf(prefix);
+    var response = await get(Uri.parse(pdf.url));
     var downloadsDirectoryPath = Platform.isAndroid
-        ? '/storage/emulated/0/Download/'
+        ? '/storage/emulated/0/Download'
         : await getDownloadsDirectory();
-    var file = File('$downloadsDirectoryPath/$prefix');
+    var file = File('$downloadsDirectoryPath/$cleanPrefix.pdf');
     var folderPath = '$downloadsDirectoryPath';
     await Directory(folderPath).create(recursive: true);
     file = await file.writeAsBytes(response.bodyBytes, mode: FileMode.write);
