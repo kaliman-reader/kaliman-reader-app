4fbc1f16012f710140a820372c5a8194e6acdab1
Misael Abanto <josemisaelabanto@gmail.com>
2024-10-16 00:20:01 -0500

    feat(backend/repository): enhance error handling in PDF fetching
    - Renamed ObjectRepository to PdfRepository for clarity
    - Added error handling logic in getPdf method
    - Logged errors using Firebase Analytics for better monitoring
    - Updated URL parameter from 'key' to 'prefix' to match API spec

--- lib/repositories/export_repository.dart
+++ lib/repositories/export_repository.dart
@@ -1,14 +1,24 @@
 import 'dart:convert';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:http/http.dart' as http;
 import 'package:kaliman_reader_app/common/constants.dart';
 import 'package:kaliman_reader_app/models/pdf.dart';
 
-class ObjectRepository {
+class PdfRepository {
   static Future<Pdf> getPdf(String prefix) async {
-    final String url = '$apiUrl/pdfs/?key=$prefix';
-    http.Response response = await http.get(Uri.parse(url));
-    Map<String, dynamic> json = jsonDecode(response.body);
-    return Pdf.fromJson(json);
+    final String url = '$apiUrl/pdfs/?prefix=$prefix';
+    try {
+      http.Response response = await http.get(Uri.parse(url));
+      Map<String, dynamic> json = jsonDecode(response.body);
+      return Pdf.fromJson(json);
+    } catch (e) {
+      FirebaseAnalytics.instance.logEvent(name: 'error', parameters: {
+        'error': 'Error fetching pdf',
+        'stack_trace': Error().stackTrace.toString(),
+        'prefix': prefix
+      });
+      throw Exception('Error fetching pdf: $e');
+    }
   }
 }
